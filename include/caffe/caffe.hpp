1e626802a02419047ca14b55ecc7cf57f185c9fb
feifeibear <fangjiarui123@gmail.com>
2017-03-14 14:03:45 +0800

    return to old data_layer

--- include/caffe/caffe.hpp
+++ include/caffe/caffe.hpp
@@ -4,7 +4,7 @@
 
 #include "caffe/blob.hpp"
 #include "caffe/common.hpp"
-//#include "caffe/filler.hpp"
+#include "caffe/filler.hpp"
 #include "caffe/layer.hpp"
 #include "caffe/layer_factory.hpp"
 #include "caffe/net.hpp"
@@ -17,5 +17,6 @@
 
 #include "caffe/LayerParameter.hpp"
 #include "caffe/NetParameter.hpp"
+//#include "caffe/proto/myproto.hpp"
 
 #endif  // CAFFE_CAFFE_HPP_
