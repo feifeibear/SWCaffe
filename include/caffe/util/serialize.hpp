4b36941674052c50a7a7248b7f2aee9e667029fb
zhangzheyu13 <zhangzheyu13@mails.tsinghua.edu.cn>
2017-04-28 07:40:03 +0800

    .

--- include/caffe/util/serialize.hpp
+++ include/caffe/util/serialize.hpp
@@ -4,6 +4,7 @@
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 #include <boost/serialization/vector.hpp>
+#include <boost/serialization/string.hpp>
 
 class Serial_Blob
 {
