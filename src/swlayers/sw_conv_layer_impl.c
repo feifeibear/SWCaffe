612e2f4ae1414214c6fb942fee0406b3a08bed08
cryinlaugh <cryinlaugh@gmail.com>
2017-07-14 09:09:25 +0800

    reduce mem consumption for matrix trans in conv layer

--- src/swlayers/sw_conv_layer_impl.c
+++ src/swlayers/sw_conv_layer_impl.c
@@ -463,18 +463,21 @@ void sw_conv_backward_pad_impl_d(
         int B,
         int pad)
 {
+	  printf("begin Backward Pad Impl\n");
 
     int cKr, cKc, cNo;
     int cRo, cCo, cB;
     int cRi, cCi, cNi;
     int Ro = Ri+2*pad-K+1 , Co = Ci+2*pad-K+1;
-    
+
     //weight_diff
     ConvData* param = (ConvData*)malloc(sizeof(ConvData));
-    Type* my_in_grad = (Type*)malloc(sizeof(Type)*Ri*Ci*Ni*B);
     Type* my_in = (Type*)malloc(sizeof(Type)*Ri*Ci*Ni*B);
+	  printf("here1 begin Backward Pad Impl\n");
     Type* my_out_grad = (Type*)malloc(sizeof(Type)*Ro*Co*No*B);
+	  printf("here2 begin Backward Pad Impl\n");
     Type* my_weight_diff = (Type*)malloc(sizeof(Type)*Ni*No*K*K);
+	  printf("here3 begin Backward Pad Impl\n");
 
     //Transformation and rot180: in (B, N, R, C) -> (R, C, N, B)
     //TODO: Can be acc with CPEs
@@ -489,7 +492,6 @@ void sw_conv_backward_pad_impl_d(
         for(cCi = 0; cCi < Ci; ++cCi)
             for(cNi = 0; cNi < Ni; ++cNi)
                 for(cB = 0; cB < B; ++cB)
-                  //my_in_grad[image_swdnn_offset_back(cB, cNi, cRi, cCi, B, Ni, Ri, Ci)] = 
                   my_in[image_swdnn_offset_back(cB, cNi, cRi, cCi, B, Ni, Ri, Ci)] = 
                     in[image_caffe_offset(cB, cNi, cRi, cCi, B, Ni, Ri, Ci)];
 #else
@@ -502,15 +504,14 @@ void sw_conv_backward_pad_impl_d(
         for(cCo = 0; cCo < Co; ++cCo)
             for(cNo = 0; cNo < No; ++cNo)
                 for(cB = 0; cB < B; ++cB)
-                  //my_out_grad[image_swdnn_offset_back(cB, cNo, cRo, cCo, B, No, Ro, Co)] = 
                   my_out_grad[image_swdnn_offset(cB, cNo, cRo, cCo, B, No, Ro, Co)] = 
                     out_grad[image_caffe_offset(cB, cNo, cRo, cCo, B, No, Ro, Co)];
 #else
-	image_caffe_to_swdnn(out_grad,my_out_grad,B, No, Ro, Co);
+	  image_caffe_to_swdnn(out_grad,my_out_grad,B, No, Ro, Co);
 #endif
 
     //memset(my_weight_diff, 0, sizeof(Type)*Ni*No*K*K);
-   
+
     param->input  = my_in;
     param->weight = my_out_grad;
     param->output = my_weight_diff;
@@ -556,8 +557,12 @@ void sw_conv_backward_pad_impl_d(
 #endif
 	  printf("Backward weight_diff OK\n");
 
+    free(my_weight_diff);
+    free(my_in);
+
     //Transforamation and rot180 for Weight
     Type* my_weight   = (Type*)malloc(sizeof(Type)*No*Ni*K*K);
+    Type* my_in_grad = (Type*)malloc(sizeof(Type)*Ri*Ci*Ni*B);
 
 #ifdef MPE_TRANS
     for(cKr = 0; cKr < K; ++cKr)
@@ -592,8 +597,8 @@ void sw_conv_backward_pad_impl_d(
     assert(Costride > 0);
 
     //memset(my_in_grad, 0, sizeof(Type)*Ni*B*Ci*Ri);
-// pad_inv(in_grad) = conv(out_grad, rot180(weight), 'full')
-	//  athread_spawn(conv_full_pad, param);
+    // pad_inv(in_grad) = conv(out_grad, rot180(weight), 'full')
+	  //  athread_spawn(conv_full_pad, param);
 	  athread_spawn(conv_full_pad,param);
     athread_join();
 #ifdef MPE_TRANS
@@ -610,9 +615,7 @@ void sw_conv_backward_pad_impl_d(
 	  printf("Backward in_grad calc is OK!\n");
 
     free(my_in_grad);
-    free(my_in);
     free(my_weight);
     free(my_out_grad);
-    free(my_weight_diff);
     free(param);
 }
