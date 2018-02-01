502ef3f6f068468d22c71d476900264c68c6fabc
cryinlaugh <cryinlaugh@gmail.com>
2018-02-01 14:18:33 +0800

    change data path to a faster dataset; change the submit queue of alexnet for 1k nodes training

--- models/alexnet/sw_runalex.sh
+++ models/alexnet/sw_runalex.sh
@@ -1,4 +1,4 @@
 #ALL_SHARE=-sw3run ./sw3run-all -sw3runarg "-a 1" -cross_size 28000 
 
-mkdir -p ./log && bsub -b -I -J sw_alex_debug -q q_sw_yfb -host_stack 1024 -N $1 -cgsp 64 -sw3run ./sw3run-all -sw3runarg "-a 1" -cross_size 28000 ./bin/caffe_sw train --solver=models/alexnet/solver_debug.prototxt 2>&1 | tee ./log/sw_alex_debug_`date '+%y-%m-%d-%H-%M-%S'`.log
+mkdir -p ./log && bsub -b -I -J sw_alex_debug -q q_sw_share -host_stack 1024 -N $1 -cgsp 64 -sw3run ./sw3run-all -sw3runarg "-a 1" -cross_size 28000 ./bin/caffe_sw train --solver=models/alexnet/solver_debug.prototxt 2>&1 | tee ./log/sw_alex_debug_`date '+%y-%m-%d-%H-%M-%S'`.log
 #mkdir -p ./log && bsub -b -I -q q_sw_yfb -host_stack 1024 -N $1 -np 1 -cgsp 64 -share_size 6000 ./bin/caffe_sw train --solver=models/alexnet/solver_debug.prototxt 2>&1 | tee ./log/sw_alex_`date '+%y-%m-%d-%H-%M-%S'`.log
